439ed0b7e41dd4a9cea94e7daa5b8da31ea046e3
MajorGamerJay <majorgamerjay@protonmail.com>
2021-06-13 00:27:56 +0600

    no comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+/* Set up logging */
 const Logging = require('./mods/Logging.js');
 const logger = new Logging.Logger(bool_fs=true);
 
-logger.logOutput("WARN", "Lol");
-logger.logOutput("ERR", "Nice error");
-logger.logOutput("INFO", "Ebic Info");
-logger.logOutput("SUCC", "BIG SUCC");
+logger.logOutput("INFO", "Started script!");
+
+/* Set up Discord Client */
+const Discord = require('discord.js');
+const client  = new Discord.Client();
+
+logger.logOutput("INFO", "Created client instance!");
+
+const config = require('../config/config.json');
+logger.logOutput("INFO", "Parsed config!");
+
+const servers = require('../config/server_list.json');
+logger.logOutput("INFO", "Parsed server list!");
+
+client.on('ready', () => {
+    logger.logOutput("SUCC", "Established connection to the client!");
+});
+
+client.login(config.token);
