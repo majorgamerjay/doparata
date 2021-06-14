f9a13fde267ca2d5f8ffd41be26bf6436b2cce0b
MajorGamerJay <majorgamerjay@protonmail.com>
2021-06-14 16:24:34 +0600

    Add collecting member list and checking algorithm

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 /* Set up logging */
-const Logging = require('./mods/Logging.js');
+const Utilities = require('./mods/Utilities');
+const Logging = require('./mods/Logging');
+
 const logger = new Logging.Logger(bool_fs=true);
 
 logger.logOutput("INFO", "Started script!");
@@ -13,11 +15,36 @@ logger.logOutput("INFO", "Created client instance!");
 const config = require('../config/config.json');
 logger.logOutput("INFO", "Parsed config!");
 
-const servers = require('../config/server_list.json');
+const servers = require('../config/server_list.json').servers;
 logger.logOutput("INFO", "Parsed server list!");
+
+if (servers.length < 2)
+    Utilities.killMyself(23, "Stuff less than 2 bruv");
 
 client.on('ready', () => {
     logger.logOutput("SUCC", "Established connection to the client!");
+
+    const serverCache = client.guilds.cache;
+
+    server_members = new Array(servers.length);
+
+    for(let k = 0; k < servers.length; ++k)
+        server_members[k] = new Array();
+
+    let i = 0;
+    servers.forEach(perServer => {
+        logger.logOutput("INFO", `Getting members of ${i}. ${perServer}`);
+        serverCache.get(perServer).members.cache.each(perMember => {
+            server_members[i].push(perMember.user.id);
+        });
+        ++i;
+    });
+
+    const accumulation = [];
+
+    Utilities.battleRoyale(server_members, accumulation);
+
+    console.log(accumulation);
 });
 
 client.login(config.token);
